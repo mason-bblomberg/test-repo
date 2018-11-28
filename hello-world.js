e1731cc0638e9e68f8f2e2fff611de23362de969
Brad Blomberg <bblomberg@masoncompaniesinc.com>
2018-11-28 09:23:53 -0600

    first draft

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
