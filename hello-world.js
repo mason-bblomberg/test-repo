73021b4d639ebc3e40dfe1c40099094a242dc12b
Brad Blomberg <bblomberg@masoncompaniesinc.com>
2018-11-28 10:59:14 -0600

    first draft

--- hello-world.js
+++ hello-world.js
@@ -1,13 +1,25 @@
 const http = require('http');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = 8080,
+    ip   = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   console.log('handling a request for %s', req.url);
+  
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  
+  var idxVerb = req.url.indexof("say_hello");
+  console.log('index of say_hello is %s', idxVerb);
+  
+  
+  if (idxVerb = 1) {
+	  var strName = req.url.substring(11);
+	  res.end('Hello, '+ strName + '\n');
+  } else {
+	  res.end('Hello, World!\n');
+  }
+  
 });
 
 server.listen(port, ip, () => {
